1cd3d1eae1f6f8e1c9fbf2be250ed72b42a4d6aa
N S Pious <nspious1999@gmail.com>
2025-02-25 18:36:04 +0530

    newinstall

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{html,ts}", // Adjust this to match the structure of your Angular project
+  
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
+  
